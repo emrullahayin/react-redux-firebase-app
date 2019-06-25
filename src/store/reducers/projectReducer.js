d2a5d6a83a63ee277562b1f47c2b4f9ee7fe8cde
Emrullah Ayın <ayinemrullah@gmail.com>
2019-06-25 17:47:30 +0300

    #14

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -37,6 +37,9 @@ const projectReducer = (state = initialState, action) => {
   switch (action.type) {
     case "CREATE_PROJECT":
       console.log("create ccc project", action.project);
+      break;
+    default:
+      return state;
   }
   return state;
 };
