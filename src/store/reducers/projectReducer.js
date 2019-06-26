b8862de9c4f6c5cdbf9729c727a29e4ece5538b9
Emrullah Ayın <ayinemrullah@gmail.com>
2019-06-26 16:25:38 +0300

    initialState

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,37 +1,4 @@
-const initialState = {
-  projects: [
-    {
-      id: 1,
-      title: "Glass Clear 7 Oz Xl",
-      content:
-        "Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.\n\nAenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum."
-    },
-    {
-      id: 2,
-      title: "Chocolate Bar - Smarties",
-      content:
-        "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\n\nEtiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.\n\nPraesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio."
-    },
-    {
-      id: 3,
-      title: "Nescafe - Frothy French Vanilla",
-      content:
-        "Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero."
-    },
-    {
-      id: 4,
-      title: "Wine - Delicato Merlot",
-      content:
-        "Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.\n\nQuisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.\n\nPhasellus in felis. Donec semper sapien a libero. Nam dui."
-    },
-    {
-      id: 5,
-      title: "Carrots - Jumbo",
-      content:
-        "Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero."
-    }
-  ]
-};
+const initialState = {};
 
 const projectReducer = (state = initialState, action) => {
   switch (action.type) {
