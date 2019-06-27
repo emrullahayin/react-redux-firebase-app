b51f9cfde1088c0f2244e28b1537328d56905e67
Emrullah Ayın <emrullahayin@gmail.com>
2019-06-27 20:24:19 +0300

    posted by

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -33,7 +33,7 @@ const ProjectDetails = ({ project }) => {
     <Grid item xs={12} className={classes.grid}>
       {project ? (
         <Card className={classes.card}>
-          <CardContent divider>
+          <CardContent >
             <Typography gutterBottom variant="h5" component="h2">
               {project.title}
             </Typography>
@@ -43,6 +43,9 @@ const ProjectDetails = ({ project }) => {
             <Divider className={classes.divider} />
             <Typography variant="overline" component="p" color="textSecondary">
               {moment.unix(project.createdAt.seconds).format("llll")}
+            </Typography>
+            <Typography variant="subtitle1" component="p" color="textSecondary">
+              Posted By: {`${project.authorFirstName} ${project.authorLastName}`}
             </Typography>
           </CardContent>
         </Card>
