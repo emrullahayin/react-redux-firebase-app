b70cee95e74fee1963800443b3f078bf197a3f6a
Emrullah Ayın <ayinemrullah@gmail.com>
2019-06-27 11:58:23 +0300

    undefined

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,26 +1,31 @@
 import React from "react";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
+
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 
 const ProjectDetails = ({ location }) => {
-  const project = location.state.project;
   return (
     <Grid item xs={12}>
-      <Card>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {project.title}
-          </Typography>
-          <Typography variant="body2" component="p">
-            {project.content}
-          </Typography>
-          <Typography variant="overline" component="p" color="textSecondary">
-            {project.time}
-          </Typography>
-        </CardContent>
-      </Card>
+      {location.state !== undefined && (
+        <Card>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {location.state.project.title}
+            </Typography>
+            <Typography variant="body2" component="p">
+              {location.state.project.content}
+            </Typography>
+            <Typography variant="overline" component="p" color="textSecondary">
+              {location.state.project.time}
+            </Typography>
+          </CardContent>
+        </Card>
+      )}
     </Grid>
   );
 };
