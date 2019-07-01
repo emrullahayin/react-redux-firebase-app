81c8427a2b9ba10266452ad534a83c04a783934e
Emrullah Ayın <ayinemrullah@gmail.com>
2019-07-01 15:20:44 +0300

    delete redux ext code

--- src/index.js
+++ src/index.js
@@ -28,8 +28,7 @@ const store = createStore(
         getFirestore
       })
     ),
-    reduxFirestore(fbConfig),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    reduxFirestore(fbConfig)
   )
 );
 
